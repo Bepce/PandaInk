00ce87cc99c93c448cb5547df71e60ada2586918
Georgi Dimitrov <bginfernali@gmail.com>
2025-05-05 13:06:26 +0300

    Added Seires Controller and first api endpoint.

---
+++ PandaInk/Controllers/SeriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PandaInk.API.Data;
+using PandaInk.API.Models;
+
+namespace PandaInk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeriesController : ControllerBase
+    {
+        private readonly PandaInkContext _context;
+
+        public SeriesController(PandaInkContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/series
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Series>>> GetSeries()
+        {
+            return await _context.Series.ToListAsync();
+        }
+    }
+}
