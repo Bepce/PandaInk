2be951efd7490e86a8eb29d25702406b01888f4d
Georgi Dimitrov <bginfernali@gmail.com>
2025-05-07 14:14:34 +0300

    Added some validation

--- PandaInk/DTOs/Review/UpdateReviewDTO.cs
+++ PandaInk/DTOs/Review/UpdateReviewDTO.cs
@@ -5,13 +5,16 @@ namespace PandaInk.API.DTOs.Review
 {
     public class UpdateReviewDTO
     {
-        
+        [Required]
         public Guid Id { get; set; }
 
+        [Required]
         public string Content { get; set; } = string.Empty;
 
+        [Required]
         public RatingEnum Rating { get; set; }
 
+        [Required]
         public Guid SeriesId { get; set; }
     }
 }
--- PandaInk/DTOs/Seires/SeriesDTO.cs
+++ PandaInk/DTOs/Seires/SeriesDTO.cs
@@ -1,21 +1,23 @@
 ﻿using PandaInk.API.DTOs.Review;
+using System.ComponentModel.DataAnnotations;
 
 namespace PandaInk.API.DTOs.Seires
 {
     public class SeriesDTO
     {
+        [Required]
         public Guid Id { get; set; }
-
+        [Required]
         public string Title { get; set; } = string.Empty;
-
+        [Required]
         public string Description { get; set; } = string.Empty;
-
+        [Required]
         public string? CoverImage { get; set; }
-
+        [Required]
         public string? Author { get; set; }
-
+        [Required]
         public DateTime? ReleaseDate { get; set; }
 
-        public List<ReviewDTO> Reviews { get; set; }
+        public List<ReviewDTO>? Reviews { get; set; }
     }
 }
